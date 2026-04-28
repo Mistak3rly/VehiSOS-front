26ad99e6bf786f00f1f005cad7a6c43c8d01e1f9
Majo <tiota8@gmail.com>
2026-04-27 20:18:07 -0400

    roles

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://vehisos-back.onrender.com',   // Base URL del backend FastAPI (sin /api/v1)
-  wsUrl: 'ws://vehisos-back.onrender.com',       // Base URL WebSocket
+  //apiUrl: 'https://vehisos-back.onrender.com',  
+  apiUrl: 'http://localhost:8000', // Base URL del backend FastAPI (sin /api/v1)
+  wsUrl: 'ws://localhost:8000',       // Base URL WebSocket
   googleMapsApiKey: 'AIzaSyBV8ErOgwiXB7jKfQO7I61pbhdwDCVPM-Q',
 };

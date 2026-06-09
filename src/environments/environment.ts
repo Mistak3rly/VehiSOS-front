8ed8937708f69d816065d1b4cdba1a84d3e5acbc
MacielRojas <fernanda.mr.inginform@gmail.com>
2026-06-09 00:17:40 -0400

    ajuste

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,11 @@
 export const environment = {
   
 
-  production: false,
-  apiUrl: 'http://localhost:8000',
-  wsUrl: 'ws://localhost:8000',  // ← agregar esto
+  production:true,
+   apiUrl: 'https://tu-backend.onrender.com/api/v1',
+  wsUrl: 'wss://tu-backend.onrender.com', 
+  //apiUrl: 'http://localhost:8000/api/v1',
+  //wsUrl: 'ws://localhost:8000',  // ← agregar esto
   useMockData: false,
   googleMapsApiKey: 'AIzaSyBV8ErOgwiXB7jKfQO7I61pbhdwDCVPM-Q',
 

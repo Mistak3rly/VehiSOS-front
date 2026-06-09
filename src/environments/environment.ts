b160f2cf0e982435f7284a22f7bf05f27119d5dd
MacielRojas <fernanda.mr.inginform@gmail.com>
2026-06-08 23:58:50 -0400

    ajuste

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,9 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8000',
-  wsUrl: 'ws://localhost:8000',
+  //production: false,
+  //apiUrl: 'http://localhost:8000',
+  //wsUrl: 'ws://localhost:8000',
+  production: true,
+  apiUrl:'https://vehisos-back.onrender.com',
   useMockData: false,
   googleMapsApiKey: 'AIzaSyBV8ErOgwiXB7jKfQO7I61pbhdwDCVPM-Q',
 };

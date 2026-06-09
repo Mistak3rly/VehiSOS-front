a7ec7eebb62cd9f10e27e367f945b072fded91d5
MacielRojas <fernanda.mr.inginform@gmail.com>
2026-06-09 00:10:21 -0400

    Corrección

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://tu-backend.onrender.com',
+  apiUrl: 'https://tu-backend.onrender.com/api/v1',
   wsUrl: 'wss://tu-backend.onrender.com',  // ← agregar esto
   useMockData: false,
   googleMapsApiKey: 'AIzaSyBV8ErOgwiXB7jKfQO7I61pbhdwDCVPM-Q'

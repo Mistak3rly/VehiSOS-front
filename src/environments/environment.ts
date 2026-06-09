12c3f2625c147d4843070a28267bdfb72edb2ace
MacielRojas <fernanda.mr.inginform@gmail.com>
2026-06-09 00:06:28 -0400

    ajustes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,10 @@
 export const environment = {
-  //production: false,
-  //apiUrl: 'http://localhost:8000',
-  //wsUrl: 'ws://localhost:8000',
-  production: true,
-  apiUrl:'https://vehisos-back.onrender.com',
+  
+
+  production: false,
+  apiUrl: 'http://localhost:8000',
+  wsUrl: 'ws://localhost:8000',  // ← agregar esto
   useMockData: false,
   googleMapsApiKey: 'AIzaSyBV8ErOgwiXB7jKfQO7I61pbhdwDCVPM-Q',
+
 };
---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://tu-backend.onrender.com',
+  wsUrl: 'wss://tu-backend.onrender.com',  // ← agregar esto
+  useMockData: false,
+  googleMapsApiKey: 'AIzaSyBV8ErOgwiXB7jKfQO7I61pbhdwDCVPM-Q'
+};
